91f2d7893bc3e008f481e686d369752c7cd3e837
mayan3259 <74152280+mayan3259@users.noreply.github.com>
2020-12-01 20:36:46 +0200

    Update Arduino.ino

--- Arduino.ino
+++ Arduino.ino
@@ -1,15 +1,40 @@
 #include "MPU9250.h"
 #include <SoftwareSerial.h>
 
-// an MPU9250 object with the MPU-9250 sensor on I2C bus 0 with address 0x68
-MPU9250 IMU(Wire,0x68);
-#define rxPin 5
-#define txPin 6
+const int rxPin = 5;
+const int txPin = 6;
 
 int status;
 float gyroX, gyroY, gyroZ;
 String str;
 SoftwareSerial espSerial(rxPin, txPin);
+
+typedef enum ESensors {
+  CENTER_BACK,
+  RIGHT_SHOULDER,
+
+  NUM_OF_CONNECTED_SENSORS,
+  
+  RIGHT_FOREARM,
+  RIGHT_WRIST,
+  LEFT_SOULDER,
+  LEFT_FOREARM,
+  LEFT_WRIST,
+  RIGHT_THIGH,  
+  RIGHT_SHIN,
+  RIGHT_ANKLE,
+  LEFT_THIGH,
+  LEFT_SHIN,
+  LEFT_ANKLE
+};
+
+MPU9250 IMU(Wire, 0x68);
+
+void TCA9548A(uint8_t bus) {
+  Wire.beginTransmission(0x70);  // TCA9548A address is 0x70
+  Wire.write(1 << bus);          // send byte to select bus
+  Wire.endTransmission();
+}
 
 void setup() {
 
@@ -23,27 +48,37 @@ void setup() {
   espSerial.begin(115200);
   delay(200);
 
-  // start communication with IMU 
-  status = IMU.begin();
-  if (status < 0) {
-    Serial.println("IMU initialization unsuccessful");
-    Serial.println("Check IMU wiring or try cycling power");
-    Serial.print("Status: ");
-    Serial.println(status);
-    while(1) {}
+  // Set communication with mux
+  Wire.begin();
+
+  // Setup sensors
+  for (int i = 0; i < static_cast<int>(NUM_OF_CONNECTED_SENSORS); i++) {
+    TCA9548A(i);
+    status = IMU.begin();
+    if (status < 0) {
+      Serial.println("IMU initialization unsuccessful");
+      Serial.println("Check IMU wiring or try cycling power");
+      Serial.print("Status: ");
+      Serial.println(status);
+      while(1) {}
+    }
   }
 }
 
 void loop() {
   // read the sensor
-  IMU.readSensor();
-  gyroX = IMU.getGyroX_rads();
-  gyroY = IMU.getGyroY_rads();
-  gyroZ = IMU.getGyroZ_rads();
-  
-  // display the data
-  str = String("X: ") + String(gyroX) + String("   ") + String("Y: ") + String(gyroY)+ String("   ") + String("Z: ") + String(gyroZ);
-  espSerial.println(str);
+  for (int i = 0; i < static_cast<int>(NUM_OF_CONNECTED_SENSORS); i++) {
+    TCA9548A(i);
 
+    IMU.readSensor();
+    gyroX = IMU.getGyroX_rads();
+    gyroY = IMU.getGyroY_rads();
+    gyroZ = IMU.getGyroZ_rads();
+  
+    // display the data
+    str = String("Sensor ") + String(i) + String(" X: ") + String(gyroX) + String("   ") + String("Y: ") + String(gyroY)+ String("   ") + String("Z: ") + String(gyroZ);
+    espSerial.println(str);
+  }
+  
   delay(50);
 }
